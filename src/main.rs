4ba77d3df7abf3f372e5daf74779573c733de3c1
brett satoru <brettsatoru@gmail.com>
2025-09-15 16:00:24 -0500

    Using Bevy Getting Started to setup a skeleton application

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
+use bevy::prelude::*;
+mod greet_system;
+use greet_system::greetsystem::HelloPlugin;
+
 fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugins(HelloPlugin)
+    .run();
 }
